e064bcd5a3271acc94adb5b3cd24e6cd6ff720bf
sandy2030 <sandeeppandey2024@gmail.com>
2024-05-14 21:00:21 +0530

    ds practice

--- com/letecode/BinarySearch.java
+++ com/letecode/BinarySearch.java
@@ -1,25 +1,35 @@
 package com.letecode;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BinarySearch {
 
-    private static int binarySearch(int[]arr,int key){
-        int low=0;int high=arr.length-1;
-        while (low<=high) {
-            int mid = (low + high) / 2;
-
-            if (key == arr[mid])
+    private static int binarySearch(int[]arr,int target){
+        int left=0; int right= arr.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if (target==arr[mid])
                 return mid;
-            else if (key < arr[mid])
-                high = mid - 1;
-            else {
-                low = mid + 1;
+            if (arr[left]<=arr[mid]){ // means left array is sorted
+                if (target<arr[left] || target>arr[mid]){ //  target is exists in right array
+                    left=mid+1;
+                }else { // target is in lest array
+                    right=mid-1;
+                }
+            }else{  // means  right array is sorted
+                if (target<arr[mid] || target>arr[right]){ // means target is in left array
+                     right=mid-1;
+                }else { //   target is in right array
+                    left=mid+1;
+                }
             }
         }
         return -1;
     }
     public static void main(String[] args) {
-        int a[]={1,4,6,7,12,23,45,56,78,90};
+        int a[]={4,5,6,7,0,1,2};
 
-        System.out.println(binarySearch(a,9));
+        System.out.println(binarySearch(a,2));
     }
 }
