2279ef0ae3afae0eb4975ec7d4b4a694fa7766dc
sandy2030 <sandeeppandey2024@gmail.com>
2025-01-25 19:19:44 +0530

    leetcode

--- com/letecode/LongestPalindromicSubstring.java
+++ com/letecode/LongestPalindromicSubstring.java
@@ -1,42 +1,45 @@
 package com.letecode;
 
 public class LongestPalindromicSubstring {
-    public String longestPalindrome(String s) {
+    public  static String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
-
-        String longest = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            // For odd-length palindromes
-            String oddPalindrome = expandAroundCenter(s, i, i);
-
-            // For even-length palindromes
-            String evenPalindrome = expandAroundCenter(s, i, i + 1);
-
-            // Update the longest palindrome
-            if (oddPalindrome.length() > longest.length()) {
-                longest = oddPalindrome;
-            }
-            if (evenPalindrome.length() > longest.length()) {
-                longest = evenPalindrome;
+        int start=0,end=0;
+        for (int i=0;i<s.length();i++) {
+            int len1 = expandFromCenter(s, i, i );
+            int len2 = expandFromCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (end-start<len){
+                start=i-(len-1)/2;
+                end=i+len/2;
             }
         }
-
-        return longest;
+        return s.substring(start,end+1);
     }
 
-    private String expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+    public static Integer expandFromCenter(String str,int left, int right) {
+        while (left>=0 && right<str.length() && str.charAt(left)== str.charAt(right)){
             left--;
             right++;
         }
-        // Return the palindromic substring found
-        return s.substring(left + 1, right);
+        return right-left-1;
     }
+static boolean isPalindrome(String str){
+    char[] charArray = str.toCharArray();
+    int len=charArray.length,i=0;
+    boolean flag=true;
+    // madxm
+    while (i<len){
+        if (charArray[i]!=charArray[len-(i+1)]){
+                flag=false;
+                break;
+        }
+        i++;
+    }
+    return flag;
+}
 
     public static void main(String[] args) {
-        LongestPalindromicSubstring solution = new LongestPalindromicSubstring();
-        String str = "babad";
-        System.out.println("Longest Palindromic Substring is: " + solution.longestPalindrome(str));
+        String str = "ac";
+        System.out.println(longestPalindrome("abcbabbabc"));
     }
 }
